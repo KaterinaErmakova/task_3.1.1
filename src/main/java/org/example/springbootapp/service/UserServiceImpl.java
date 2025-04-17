81f9e3d89b618ea2e4ff5949ddadee51601b2c92
Katerina_Ermakova <katrin6793@gmail.com>
2025-04-17 17:59:28 +0300

    Changes: added exception handling in the UserService

--- src/main/java/org/example/springbootapp/service/UserServiceImpl.java
+++ src/main/java/org/example/springbootapp/service/UserServiceImpl.java
@@ -4,7 +4,9 @@ import jakarta.transaction.Transactional;
 import org.example.springbootapp.dao.UserDao;
 import org.example.springbootapp.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 
 import java.util.*;
@@ -41,6 +43,9 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public void updateUser(int id, User user) {
         User updatedUser = userDao.getUserById(id);
+        if (updatedUser == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User with id " + id + " not found");
+        }
         updatedUser.setFirstName(user.getFirstName());
         updatedUser.setSecondName(user.getSecondName());
         updatedUser.setAge(user.getAge());
